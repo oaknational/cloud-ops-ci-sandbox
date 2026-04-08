998414a4531563946b1853389efd09e9d0e243d5
Rob Ward <rob.ward@thenational.academy>
2026-04-08 15:44:35 +0100

    test: module commit hash

--- infrastructure/project/main.tf
+++ infrastructure/project/main.tf
@@ -1,5 +1,5 @@
 module "vercel" {
-  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.6.0"
+  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=5b55cf17c6d84ee33e6b6586062a0444a37377f5" # v1.6.0
   build_type                       = "function"
   cloudflare_zone_domain           = "domain"
   environment_variables            = []
