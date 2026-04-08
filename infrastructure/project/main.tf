48b54956940f8797777dcb2c73e67b9beac16a9f
Rob Ward <rob.ward@thenational.academy>
2026-04-08 15:28:24 +0100

    feat: add dummy terraform

---
+++ infrastructure/project/main.tf
@@ -0,0 +1,11 @@
+module "vercel" {
+  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.6.0"
+  build_type                       = "function"
+  cloudflare_zone_domain           = "domain"
+  environment_variables            = []
+  framework                        = "other"
+  project_visibility               = "private"
+  git_repo                         = "oaknational/cloud-ops-ci-sandbox"
+  protection_bypass_for_automation = false
+  skew_protection                  = "1 day"
+}
